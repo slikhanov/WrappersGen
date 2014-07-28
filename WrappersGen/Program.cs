5f616dbd4de0e37049a5f694490f9c235edb80aa
Brrrutal <slikhan@gmail.com>
2014-07-28 18:47:12 -0400

    Some experiments with libclang

--- WrappersGen/Program.cs
+++ WrappersGen/Program.cs
@@ -10,6 +10,30 @@ namespace WrappersGen
     {
         static void Main(string[] args)
         {
+            ClangSharp.Index idx = new ClangSharp.Index();
+            var options = new[] 
+            {
+                @"-x",
+                @"c++",
+                @"-ID:\VQ\perforce\slikhano_wsi_vq_ws\Azure\include\WxKernel"
+            };
+
+            var translationUnit = idx.CreateTranslationUnit(
+                    @"D:\VQ\perforce\slikhano_wsi_vq_ws\Azure\include\WxKernel\Builder\Scene\WxdDataView.h",
+                    options);
+            foreach (var diag in translationUnit.Diagnostics)
+            {
+                System.Console.WriteLine(diag.Description);
+            }
+            var c = translationUnit.Cursor;
+            c.VisitChildren((cursor, parent) => 
+            {
+                if (cursor.DisplayName == @"WxdDataView")
+                {
+                    System.Console.WriteLine(cursor.Definition);
+                }
+                return ClangSharp.Cursor.ChildVisitResult.Continue;
+            });
         }
     }
 }
